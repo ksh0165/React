a8240d9bfc0724adf3f9773572fc68e81837a78e
김재성 <wkssm7575@naver.com>
2022-04-04 13:02:27 +0900

    movie detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,20 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import {useNavigate,useLocation} from 'react-router-dom';
+import './Detail.css';
+
 const Detail = () => {
-    const {location} = useLocation;
+    const location = useLocation();
+    const [detail,setDetail] = useState(location.state);
     const {navigate} = useNavigate;
-    console.log(navigate);
-    console.log(location);
 
     useEffect(()=>{
-        if(location.state === undefined){
+        if(location === undefined){
             navigate("/");
         }
     },[])
     return (
         <>
-            {location.state ? (
-                <span>{location.state.title}</span>
+            {detail ? (
+                <div className="movieDetail">
+                <img className="movie__image" src={detail.poster} alt={detail.title} title={detail.title} />
+                <div className="movie__data">
+                    <h3 className="movie__title">{detail.title}</h3>
+                    <h5 className="movie__year">{detail.year}</h5>
+                    <ul className="movie__genres">
+                        {detail.genres.map((genre,index) => {
+                            <li key={index} className="movie__genre">
+                                {genre}
+                            </li>
+                        })}
+                    </ul>
+                    <p className="movie__summary">{detail.summary}</p>
+                </div>
+            </div>
             ):(
                 null
             )}
